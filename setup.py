98a4a5afb97cb9c1dcacdf458644901479bc5eae
Jérôme Blondon <jerome@yasound.com>
2012-02-24 07:57:43 +0100

    Added packaging support
    * added setup.py and other needed files (empty LICENSE, HISTORY.markdown, ..)
    
    The key idea is to allow this project to be included in yaapp.requirement.txt in order to reuse code for importing songs without metadata.

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+required = ['mutagen',
+            'psycopg2',
+            'requests',]
+packages = [
+    'uploader',
+]
+
+setup(
+    name='uploader',
+    version=uploader.__version__,
+    description='Uploader for Yasound.',
+    long_description=open('README.markdown').read() + '\n\n' +
+                     open('HISTORY.markdown').read(),
+    author='Jérôme Blondon',
+    author_email='jerome@yasound.com',
+    url='https://github.com/yasound/uploader',
+    packages=packages,
+    package_data={'': ['LICENSE', 'NOTICE']},
+    include_package_data=True,
+    install_requires=required,
+    license='Proprietary',
+    classifiers=(
+    ),
+)
+
+
